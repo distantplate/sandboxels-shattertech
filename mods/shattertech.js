1a5ad0b358d344d1e5275310042630b0895c8e1a
distantplate <philiphslade478@gmail.com>
2025-11-25 10:09:54 -0500

    Update shattertech.js

--- mods/shattertech.js
+++ mods/shattertech.js
@@ -1387,8 +1387,8 @@ function isObjValDupe(obj,vals) {
 
 function shieldcheck(x,y,radius) {
   if (!storageList.shield_gen) {return false;}
-  scc = {};
-  sfc = {};
+  var scc = {};
+  var sfc = {};
   var shieldCloseCheck = false;
   var shieldFarCheck = false;
   for (let d in storageList.shield_gen) {
