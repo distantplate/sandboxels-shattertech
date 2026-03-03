fc943f942f2849e1b83754bdf84f07cf77f68608
distantplate <philiphslade478@gmail.com>
2026-03-03 08:04:23 -0500

    Update shattertech.js

--- mods/shattertech.js
+++ mods/shattertech.js
@@ -1462,19 +1462,19 @@ function shieldcheck(x,y,radius,doDamage) {
       }
     }
     //stage 2
-    /*var sc2 = {c: [],f: []};
+    var sc2 = {c: [],f: []};
     for (let a in sc1) {
       for (let b in sc1[a]) {
         var x1 = sc1[a][b].x;
         var y1 = sc1[a][b].y;
         var p = pixelMap[x1][y1];
-        if (p.offline == false && p.timer == 0 && p.syncCheck == 10) {
+        /*if (p.offline == false && p.timer == 0 && p.syncCheck == 10) {
           if (nestList.t[x1][y1]) == true && !nestList.f[x1][y1]) {
             sc2[a].push({x: x1,y: y1});
           }
-        }
+        }*/
       }
-    }*/
+    }
     //stage 3 - "your scientists were so procupied with whether or not they could that they didn't stop to think if they should"
     /*var sc3 = {c: [],f: []};
     for (let a in sc2) {
