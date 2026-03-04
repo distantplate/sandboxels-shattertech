4d315e522957e0eed6f4ccbf524f62845d0a318f
distantplate <philiphslade478@gmail.com>
2026-03-04 12:27:03 -0500

    Update shattertech.js

--- mods/shattertech.js
+++ mods/shattertech.js
@@ -1527,9 +1527,8 @@ function shieldcheck(x,y,radius,doDamage) {
         }
       }
     }
-    //var tempobj = {sCC: sc3.c,sCVal: shieldCloseCheck,sFC: sc3.f,sFVal: shieldFarCheck};
-    //return tempobj;
-    return false;
+    var tempobj = {sCC: sc3.c,sCVal: shieldCloseCheck,sFC: sc3.f,sFVal: shieldFarCheck};
+    return tempobj;
   } else {return false;}
 };
 
