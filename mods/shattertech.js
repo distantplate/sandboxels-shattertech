08f4b80f490592186fcbc2890106bb8d4f9bf9bf
distantplate <philiphslade478@gmail.com>
2026-05-14 12:36:15 -0400

    Update shattertech.js

--- mods/shattertech.js
+++ mods/shattertech.js
@@ -1557,10 +1557,11 @@ explodeAt = function(x,y,radius,fire="fire") {
     for (var i = 0; i < coords.length; i++) {
         bypass = false;
         if (radius <= 30 && bypass == false && checks !== false) {
-            if (pixelMap[coords[i].x][coords[i].y].element === "shield_gen") {bypass = true;}
             if (checks.sCVal == true) {
                 for (let z in checks.sCC){
-                    if (!checks.sCC[z].f) {
+                    if (coords[i].x == checks.sCC[z].x && coords[i].y == checks.sCC[z].y) {
+                        bypass = true;
+                    } else if (!checks.sCC[z].f) {
                         var tSCC = checks.sCC[z];
                         if (findFociDistance(coords[i].x,tSCC.fx1,tSCC.fx2,coords[i].y,tSCC.fy1,tSCC.fy2) > tSCC.d){
                             bypass = true;
@@ -1723,7 +1724,6 @@ runEveryTick(function () {
 runAfterReset(function() {
     storageList = {};
 });
-
 runAfterLoad(function() {
     storageList = {};
 });
