775b2b4418068c2f1e4e5b7d280067b477bed024
distantplate <philiphslade478@gmail.com>
2026-06-08 09:53:51 -0400

    Update shattertech.js

--- mods/shattertech.js
+++ mods/shattertech.js
@@ -516,17 +516,17 @@ elements.net_link = {
                         if (newPixel.active > 0) {
                           var list1 = [pixel.netConflict[0],pixel.netConflict[1],pixel.coreLoc[0],pixel.coreLoc[1]];
                           var list2 = [newPixel.netConflict[0],newPixel.netConflict[1],newPixel.coreLoc[0],newPixel.coreLoc[1]];
-                          var check = 1;
+                          var c = 1;
                           for (i = 0; i < 4; i++) {
-                            if (check == 1) {
-                              if (list1[i] > list2[i]) {check = 2;}
-                              else if (list1[i] < list2[i]) {check = 3;}
+                            if (c == 1) {
+                              if (list1[i] > list2[i]) {c = 2;}
+                              else if (list1[i] < list2[i]) {c = 3;}
                             }
                           }
-                          logMessage(check);
+                          logMessage(c);
                           //somehow this single if statment (the commented one) is causing it to break in tests.
                           //this is despite the fact that it shouldn't even get to this point in said tests.
-                          if (check == 2) {continue;}
+                          if (c == 2) {continue;}
                           //continue;
                         } else if (newPixel.active == 1) {continue;}
                       }
