a3e94e68584fb07a14ff07aa50c9bbd6a769aabd
distantplate <philiphslade478@gmail.com>
2026-07-09 10:44:34 -0400

    Update shattertech.js

--- mods/shattertech.js
+++ mods/shattertech.js
@@ -154,7 +154,7 @@ elements.emitter = {
           if (isEmpty(pixel.link[0],pixel.link[1],true)) {pixel.link = false;}
           else if (pixelMap[pixel.link[0]][pixel.link[1]].element !== "net_core") {pixel.link = false;}
           else if (pixelMap[pixel.link[0]][pixel.link[1]].fault === true) {pixel.link = false;}
-          if (pixelMap[pixel.link[0]][pixel.link[1]].augList.beam_overclocker) {overclocked = true;}
+          else if (pixelMap[pixel.link[0]][pixel.link[1]].augList.beam_overclocker) {overclocked = true;}
         }
         if (pixel.buffer){
             if (!pixel.charge) {
@@ -608,12 +608,12 @@ elements.net_link = {
               if (pixel.active > 1 && pixel.activeStart != pixelTicks) {pixel.active--;}
               if (pixel.detection.includes(2)) {
                 pixel.stage = 3;
-                /*if (pixel.active > 0 && pixel.coreLoc) {
+                if (pixel.active > 0 && pixel.coreLoc) {
                   var target = pixelMap[pixel.coreLoc[0]][pixel.coreLoc[1]];
                   if (!isEmpty(target.x,target.y,true) ? (target.element === "net_core") : false) {
                     target.fault = true;
                   }
-                }*/
+                }
                 newColor = "#360036";
               } else if (pixel.temp > 10000) {
                 newColor = "#9b00ff"
