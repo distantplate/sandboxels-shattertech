f9c50ef70ef133d6929fea976cccdb89fad501c7
distantplate <philiphslade478@gmail.com>
2026-09-03 11:52:45 -0400

    Update shattertech.js

--- mods/shattertech.js
+++ mods/shattertech.js
@@ -917,7 +917,7 @@ elements.shield_gen = {
                 }
                 if ((targetloc.xStage + targetloc.gap) <= pixel.xStage) {
                     if ((targetloc.yStage + targetloc.gap) <= pixel.yStage) {
-                        var p = pixelMap[storageList.shield_gen[a].x][storageList.shield_gen[a].x];
+                        var p = pixelMap[storageList.shield_gen[a].x][storageList.shield_gen[a].y];
                         if (Math.pow((p.x-pixel.x)/pixel.xStage,2)+Math.pow((p.y-pixel.y)/pixel.yStage,2) <= 1) {
                           inList.push({x: storageList.shield_gen[A].x,y: storageList.shield_gen[A].y});
                         }
