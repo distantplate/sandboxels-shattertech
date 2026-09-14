39f9511e54713f805690f949f4dcb16140a4ae24
distantplate <philiphslade478@gmail.com>
2026-09-14 10:43:17 -0400

    Update shattertech.js

--- mods/shattertech.js
+++ mods/shattertech.js
@@ -1149,8 +1149,10 @@ elements.disintegrate = {
             var spots = [-1,0,1];
             var moved = false;
             shuffleArray(spots);
-            for (var i = 0; i < spots.length; i++) {
-                if (tryMove(pixel,pixel.x+spots[i],pixel.y+1)) {moved = true;}
+            if (moved !== true) {
+                for (var i = 0; i < spots.length; i++) {
+                    if (tryMove(pixel,pixel.x+spots[i],pixel.y+1)) {moved = true;}
+                }
             }
             if (moved !== true) {
                 var dir = Math.random() < 0.5 ? 1 : -1;
