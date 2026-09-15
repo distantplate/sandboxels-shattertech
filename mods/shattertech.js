107b18ce3a3776eed390ec347307ef75a624ac2d
distantplate <philiphslade478@gmail.com>
2026-09-15 10:20:04 -0400

    Update shattertech.js

--- mods/shattertech.js
+++ mods/shattertech.js
@@ -121,10 +121,9 @@ elements.melt_bomb = {
             coords.forEach(function(coord){
                 var x = coord.x;
                 var y = coord.y;
-                if (isEmpty(x,y,true)) {
-                    logMessage("test1");
+                if (!isEmpty(x,y,true)) {
+                    logMessage("test");
                 }
-                //if (isEmpty(x,y,true)) {continue;}
                 /*var p = pixelMap[x][y];
                 if (p.state === "liquid") {continue;}
                 else if (p.hardness ? p.hardness == 1 : false) {continue;}
